b685f074219706a7e649da87fa72af6cfee71a5b
ruben <ruben@genohm.com>
2016-07-29 14:41:48 +0200

    Adds skeleton for web application (cookbook)
    Change-Id: I3b14ffc1f759dbe70d35f3caf69e3267ebc90ec3

---
+++ genohm/cookbook/order-submission/main.py
@@ -0,0 +1,35 @@
+import web
+import hashlib
+import uuid
+from web import form
+
+render = web.template.render('genohm/cookbook/order-submission/templates/')
+
+register_form = form.Form(
+    form.Textbox("Project", description="Project"),
+    form.Button("submit", type="submit", description="Register"),
+)
+
+urls = (
+  '/', 'CreateOrder',
+)
+
+
+class CreateOrder:
+
+    def GET(self):
+        f = register_form()
+        return render.add_order(f)
+
+    def POST(self):
+        f = register_form()
+        if not f.validates():
+            return render.add_order(f)
+        else:
+            # Put Slims code here
+            return render.add_order_success()
+
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()

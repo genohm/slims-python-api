f3aad41e71494fcb1d15f0673c55fa8dfea62b38
stijn <stijn@genohm.com>
2018-12-06 18:43:10 +0100

    SLIMS-12128 SLIMS python api: add instructions on how to download a document with a slimsgate flow
    Change-Id: Id3b402f8541103ebf0c3888eb5764fcdfd9f5010

---
+++ cookbook/flow-example/downloading_file_with_SLIMS_GATE.py
@@ -0,0 +1,25 @@
+from slims.slims import Slims
+from slims.step import Step, file_output
+from slims.output import file_value
+
+
+def execute():
+    # Make sure the path to the file exists
+    return file_value('C:/Users/User/Downloads/file.txt')
+
+
+slims = Slims("slims", url="http://127.0.0.1:9999/", token="devToken", local_host="0.0.0.0", local_port=5000)
+
+
+slims.add_flow(
+    flow_id="FLOW_0001",
+    name="Download an file from server",
+    usage="CONTENT_MANAGEMENT",
+    steps=[
+        Step(
+            name="This downloads a file.",
+            action=execute,
+            output=[
+                file_output()
+            ])
+    ])
